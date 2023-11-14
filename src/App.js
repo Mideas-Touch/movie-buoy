69ae92146dc1b9c748610012cf981dac6d9a5021
Midas-Touch <wickynjy@gmail.com>
2023-11-14 13:18:21 -0500

    create directory structure

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
+import Navbar from "./Components/Navbar";
+
 function App() {
   return (
-    <div className="App">
-      <div>
-        <h1>Hello World!</h1>
-      </div>
-    </div>
+    <>
+      <Navbar />
+    </>
+    
   );
 }
 
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -2,6 +2,24 @@ import React from "react";
 
 function Navbar(){
 
+    return <>
+        <div id="Navbar">
+            <h1 
+                className="logo"
+                >
+                    MovieBuoy
+            </h1>
+            <form
+                className="Searchbox"
+            >
+                <input 
+                    type="text" 
+                    name="search" 
+                    placeholder="Search..." />
+            </form>
+        </div>
+    </>
+
 }
 
 export default Navbar;
