78a4da160a6d6f38dcbfce07390daeb17583c0da
Midas-Touch <wickynjy@gmail.com>
2023-11-21 13:45:56 -0500

    handle errors, load valid content only

--- src/Components/Moviecard.js
+++ src/Components/Moviecard.js
@@ -8,20 +8,26 @@ import { CiBookmarkPlus } from "react-icons/ci";
 
 function Moviecard(){
     const [movies, setMovies] = useState([]);
+    const [errors, setErrors] = useState(null)
 
     let idsArray = [];
 
     const generateRandomIds = (arr)=>{
-        for (let i = 0; i < 20; i++) {
+        for (let i = 0; i < 10; i++) {
             arr.push(Math.floor(Math.random() * 100) + 1);
         }
         console.log(idsArray)
+
+        if (idsArray.length >= 10){
+            // console.log(idsArray)
+            batchLoadMovies(idsArray)
+        }
     }
 
 
     const batchLoadMovies = (ids) =>{
             ids?.forEach(element => {
-                console.log(element)
+                // console.log(element)
                 getMovies(element)
             }); 
     }
@@ -29,20 +35,35 @@ function Moviecard(){
    
     useEffect(()=>{
         generateRandomIds(idsArray)
-        batchLoadMovies(idsArray)
-    })
+    }, [])
 
 
     async function getMovies(id){
         const response = await fetch(`https://api.themoviedb.org/3/movie/${id}?api_key=6b8f7686219cfd2cee079462061c954e`)
+            .catch(error =>{
+                setErrors(errors)
+            })
+
         const data = await response.json()
-        console.log(data)
-        // const movies = [...Array(5)].map((_, i) =>({
-        //     movieId: data.id
-        // }
-        // ))
-        setMovies(movies)
-        console.log(movies)
+        // console.log(data)
+        if (data.id){
+            const movies = [...Array(1)].map((_, i) =>({
+                movieId: data.id,
+                date: data.release_date,
+                title: data.original_title,
+                description: data.overview,
+                rating: data.popularity,
+                poster: data.poster_path,
+                time: data.runtime,
+                average_vote: data.vote_average
+    
+            }
+            ))
+            setMovies(movies)
+            console.log(movies)
+        }  
+        
+        
     }
 
     return <>
