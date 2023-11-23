0f0c6d328d0dbb8a0148c6b339e9eec67ad069c4
Midas-Touch <wickynjy@gmail.com>
2023-11-23 03:00:50 -0500

    move a few things, work on list rendering

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import Navbar from "./Components/Navbar";
 import Search from "./Components/Search";
-import Movies from "./Pages/Movies"
+import Movies from "./Components/Movies"
 
 function App() {
   return (
--- src/Components/Moviecard.js
+++ src/Components/Moviecard.js
@@ -1,77 +1,83 @@
 import React, {  useEffect, useState } from "react";
-// import image_1 from "../images/image_1.jpeg"
+import image_1 from "../images/image_1.jpeg"
 import "../css/Moviecard.css";
 import { MdFavoriteBorder } from "react-icons/md"
 import { CiStar } from "react-icons/ci";
 import { CiPlay1 } from "react-icons/ci";
 import { CiBookmarkPlus } from "react-icons/ci";
+import Movies from "./Movies";
 
-function Moviecard(){
-    const [movies, setMovies] = useState([]);
-    const [errors, setErrors] = useState(null)
+function Moviecard(movies){
+    // const [movies, setMovies] = useState([]);
+    // const [errors, setErrors] = useState(null)
 
-    let idsArray = [];
+    // let idsArray = [];
 
-    const generateRandomIds = (arr)=>{
-        for (let i = 0; i < 10; i++) {
-            arr.push(Math.floor(Math.random() * 100) + 1);
-        }
-        console.log(idsArray)
+    // const generateRandomIds = (arr)=>{
+    //     for (let i = 0; i < 10; i++) {
+    //         arr.push(Math.floor(Math.random() * 100) + 1);
+    //     }
+    //     console.log(idsArray)
 
-        if (idsArray.length >= 10){
-            // console.log(idsArray)
-            batchLoadMovies(idsArray)
-        }
-    }
+    //     if (idsArray.length >= 10){
+    //         // console.log(idsArray)
+    //         batchLoadMovies(idsArray)
+    //     }
+    // }
 
 
-    const batchLoadMovies = (ids) =>{
-            ids?.forEach(element => {
-                // console.log(element)
-                getMovies(element)
-            }); 
-    }
+    // const batchLoadMovies = (ids) =>{
+    //         ids?.forEach(element => {
+    //             // console.log(element)
+    //             getMovies(element)
+    //         }); 
+    // }
 
    
-    useEffect(()=>{
-        generateRandomIds(idsArray)
-    }, [])
+    // useEffect(()=>{
+    //     generateRandomIds(idsArray)
+    // }, [])
 
 
-    async function getMovies(id){
-        const response = await fetch(`https://api.themoviedb.org/3/movie/${id}?api_key=6b8f7686219cfd2cee079462061c954e`)
-            .catch(error =>{
-                setErrors(errors)
-            })
+    // async function getMovies(id){
+    //     const response = await fetch(`https://api.themoviedb.org/3/movie/${id}?api_key=6b8f7686219cfd2cee079462061c954e`)
+    //         .catch(error =>{
+    //             setErrors(errors)
+    //         })
 
-        const data = await response.json()
-        // console.log(data)
-        if (data.id){
-            const movies = [...Array(1)].map((_, i) =>({
-                movieId: data.id,
-                date: data.release_date,
-                title: data.original_title,
-                description: data.overview,
-                rating: data.popularity,
-                poster: data.poster_path,
-                time: data.runtime,
-                average_vote: data.vote_average
+    //     const data = await response.json()
+    //     // console.log(data)
+    //     if (data.id){
+    //         const movies = [...Array(1)].map((_, i) =>({
+    //             movieId: data.id,
+    //             date: data.release_date,
+    //             title: data.original_title,
+    //             description: data.overview,
+    //             rating: data.popularity,
+    //             poster: data.poster_path,
+    //             time: data.runtime,
+    //             average_vote: data.vote_average
     
-            }
-            ))
-            setMovies(movies)  
-            console.log(movies)
+    //         }
+    //         ))
+    //         console.log(movies)
+    //         setMovies(movies)  
             
-        } 
+    //     } 
         
         
-    }
+    // }
+
+    const cardList = () => movies?.map(movie => {
+        return <Moviecard key={movie.movieId} date={movie.date} title={movie.title} description={movie.description} rating={movie.rating} 
+        poster={movie.poster} time={movie.time} average_vote={movie.average_vote}/>
+    })
 
     return <>
-    <div className="card">
+    <Moviecard className="card">
         <img
             id="poster"
-            src={movie.poster}
+            src={poster}
             alt="placeholder"
             >
         </img>
@@ -81,7 +87,7 @@ function Moviecard(){
                 <CiStar />
                 <MdFavoriteBorder />
             </div>
-            <h2>Movie Title</h2>
+            <h2>{title}</h2>
             <div
                 className="buttons"
             >
@@ -98,7 +104,7 @@ function Moviecard(){
                 </button>
 
             </div>
-    </div>
+    </Moviecard>
        
     </>
 }
